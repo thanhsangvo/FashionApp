961e3fcd2605be411e91b5275aa5e5a2382dfe86
thanhsangvo <pl.thanhsang0710@gmail.com>
2020-07-12 15:30:19 +0700

    Initial Commit

---
+++ FashionApp/FashionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FashionAppApp.swift
+//  FashionApp
+//
+//  Created by Võ Thanh Sang on 7/12/20.
+//
+
+import SwiftUI
+
+@main
+struct FashionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
